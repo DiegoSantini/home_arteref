e153c647da5cdfbaa3144f8b1d3bfe313ef18dc8
Diego Silva Santini dos Santos <didisantini@gmail.com>
2019-04-18 13:52:43 -0300

    Ultima versão

--- home_arteref_init.php
+++ home_arteref_init.php
@@ -14,7 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 //carrega template da página home 
 require 'content/home.php';
-//require 'render/ajax.php';
 require 'render/get_posts.php';
 
 function home_enqeue_scripts(){
@@ -22,8 +21,10 @@ function home_enqeue_scripts(){
     if ( is_page($page->ID) ){
         wp_enqueue_style( 'cdn_bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
         wp_enqueue_style( 'home_arteref_style', plugins_url('home-arteref/content/css/style.css'));
+        wp_enqueue_style( 'home_arteref_style_scroll', plugins_url('home-arteref/content/css/scroll_style.css' ));
         wp_enqueue_style( 'cdn_bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js','jquery', 1.4, true);
         wp_enqueue_script('home_arteref_script_slider', plugins_url('home-arteref/content/js/slider.js'),'jquery', 1.4, true);
+        wp_enqueue_script('home_arteref_click_and_drag_scroll', plugins_url('home-arteref/content/js/click_and_drag_scroll.js'),'jquery', 1.4, true);
     }
 }
 add_action('wp_enqueue_scripts', 'home_enqeue_scripts');
